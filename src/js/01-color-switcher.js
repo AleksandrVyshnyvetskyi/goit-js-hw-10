d623a21f3cb494874b7d8b77a6a585c2b432f03d
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-08-12 18:39:57 +0300

    Task 3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -8,6 +8,10 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+function getRandomHexColorTwo() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let intervalId = null;
@@ -15,7 +19,7 @@ let intervalId = null;
 const startBtnClick = () => {
     console.log('+1 click to startBtn')
     intervalId = setInterval(() => {
-        document.body.style.backgroundColor = getRandomHexColor();
+        document.body.style.background = `-webkit-linear-gradient(45deg,${getRandomHexColor()},${getRandomHexColorTwo()})`;
     }, 1000);
     startBtn.setAttribute('disabled', 'true');
     stopBtn.removeAttribute('disabled', 'true');
