69794431181a5f3d37e509cab3350440655a6995
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-08-18 16:45:24 +0300

    up

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ import Notiflix from 'notiflix'
 export const fetchCountries = name => {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => { 
         if (!response.ok) {
-            Notiflix.Notify.info("🤖 Too many matches found. Please enter a more specific name 🤖")
+            Notiflix.Notify.failure("😱 Oops, there is no country with that name 😱");
             return Promise.reject('Это ошибка 404') ;
         }  
         return response.json();
